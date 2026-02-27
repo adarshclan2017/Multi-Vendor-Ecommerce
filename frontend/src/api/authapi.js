a3d8084d0021bfb469804fe701a40b28534c11fd
ADARSH <adarshclan2017@gmail.com>
2026-02-27 14:49:10 +0530

    caps updated

--- frontend/src/api/authapi.js
+++ frontend/src/api/authapi.js
@@ -1,9 +1,9 @@
 import API from "./apiClient";
 
 export const loginuser = (data) => {
-  return API.post("/auth/Login", data);
+  return API.post("/auth/login", data);
 };
 
 export const registeruser = (data) => {
-  return API.post("/auth/Register", data);
+  return API.post("/auth/register", data);
 };
